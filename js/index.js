b1dc61391fbddaaf1a8090fe7abf34d93fcf7e74
Mikey Williams <manicdesigner@gmail.com>
2020-11-10 02:47:41 -0500

    add nav section

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navs = document.querySelectorAll('nav a');
+navs.forEach((nav, i) => {
+    nav.textContent = siteContent['nav'][`nav-item-${i + 1}`];
+    
+})
